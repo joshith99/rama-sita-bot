73da5d1be5d8bb68ec6ab740b004a3e9fbc9a8de
Joshith <72139749+joshith99@users.noreply.github.com>
2022-12-19 21:30:25 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from os import environ
+
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+load_dotenv()
+token = environ["TOKEN"]
+client.run(token)
